fc51a87303238e7fa0bc7147551936d8e6f0abed
ellipsclamation <seppcness@gmail.com>
2016-10-31 14:38:56 -0700

    completed machine usage
    fixed elliptical machine fx:id in MyGym.fxml
    fixed MyGymController to pull from the machineusage database instead of
    gymequip
    created EquipmentType object to handle data for the chart
    fixed pie chart to display equipment that are in use and available
    instead of hard coded values from database
    bound each image from machine usage display to their respective
    GymEquipment
    set background color to each image in machine usage to display its state
    
    placed some of the code into their own functions

--- CS 3337/TGEU/src/TGEU/GymEquipment.java
+++ CS 3337/TGEU/src/TGEU/GymEquipment.java
@@ -1,35 +1,69 @@
 package TGEU;
 
+import javafx.scene.image.ImageView;
+
 public class GymEquipment {
 	private int id;
-	private String name;
-	private int numb;
+	private String type;
+	private String equipment;
+	private int state;
+	private ImageView icon;
 
-	public GymEquipment(int id, String name, int numb) {
+	public GymEquipment(int id, String type, String equipment, int state, ImageView icon) {
 		super();
 		this.id = id;
-		this.name = name;
-		this.numb = numb;
+		this.type = type;
+		this.equipment = equipment;
+		this.state = state;
+		this.icon = icon;
 	}
-	
+
+	GymEquipment(int id, String type, String equipment, int state) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.equipment = equipment;
+		this.state = state;
+	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
+
+	public String getType() {
+		return type;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setType(String type) {
+		this.type = type;
 	}
-	public int getNumb() {
-		return numb;
+
+	public String getEquipment() {
+		return equipment;
 	}
-	public void setNumb(int numb) {
-		this.numb = numb;
+
+	public void setEquipment(String equipment) {
+		this.equipment = equipment;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
 	}
 	
-	
+	public ImageView getIcon() {
+		return icon;
+	}
+
+	public void setIcon(ImageView icon) {
+		this.icon = icon;
+	}
+
 }
