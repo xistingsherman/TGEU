95f650da4b4ed49d3a2a977effe8759f2cad79ce
ellipsclamation <seppcness@gmail.com>
2016-10-30 17:15:28 -0700

    updated connection with MyGymController to sql database
    added GymEquipment class
    using mysql-connector-java-5.1.38-bin.jar

--- CS 3337/TGEU/src/TGEU/MyGymController.java
+++ CS 3337/TGEU/src/TGEU/MyGymController.java
@@ -2,9 +2,14 @@ package TGEU;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -25,31 +30,53 @@ public class MyGymController  implements Initializable{
 	private Button homeButton;
 	@FXML
 	private PieChart chartOfPie;
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
-	//TEST
+	
+	//gymequip machineusage GED
+	public ArrayList<GymEquipment> gymEquip(){
+		ArrayList<GymEquipment> equipments = new ArrayList<GymEquipment>();
+		Connection c = null;
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://192.241.213.248/GED";
+	        String username = "allen3just";
+	        String password = "Justanswer30!";
+	        
+	        c = DriverManager.getConnection( url, username, password );
+	        Statement stmt = c.createStatement();
+	        ResultSet rs = stmt.executeQuery( "select * from gymequip;" );
+	        
+	        while( rs.next() ){
+            	
+                GymEquipment equipment = new GymEquipment( rs.getInt( "id" ),
+                    rs.getString( "name" ), rs.getInt( "numb" ));
+                
+                equipments.add( equipment );
+            }
+	        
+		}
+		catch( Exception e ){
+			e.printStackTrace();
+	    }
+		
+		return equipments;
+	}
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		ArrayList<GymEquipment> equipments = new ArrayList<GymEquipment>();
+		try {
+			Collections.copy(equipments, gymEquip());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		ObservableList<PieChart.Data> pieChartData =
 	            FXCollections.observableArrayList(
-	            new PieChart.Data("Cycling Machine (3)", 3),
-	            new PieChart.Data("Tread Mill (3)", 2),
+	            new PieChart.Data(equipments.get(0).getName() + "(" + equipments.get(0).getNumb() + ")", equipments.get(0).getNumb()),
+	            new PieChart.Data("Treadmill (3)", 2),
 	            new PieChart.Data("Stair Master (2)", 2),
-	            new PieChart.Data("Eliptical Machine(3)", 3),
+	            new PieChart.Data("Elliptical Machine(3)", 3),
 				new PieChart.Data("Free (2)", 2));
 	    chartOfPie.setData(pieChartData);
 	    chartOfPie.setLabelsVisible(false);
